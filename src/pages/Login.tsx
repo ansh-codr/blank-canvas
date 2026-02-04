3fe77df80a12f40d22c6c70817b22b9dc720b337
ansh-codr <anshloverao@gmail.com>
2026-02-04 22:53:39 +0530

    Fix AuthPage imports to use default export

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { AuthPage } from "@/components/ui/auth-page";
+import AuthPage from "@/components/ui/auth-page";
 
 export const Login = () => {
   return <AuthPage mode="login" />;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import { AuthPage } from "@/components/ui/auth-page";
+import AuthPage from "@/components/ui/auth-page";
 
 export const Register = () => {
   return <AuthPage mode="register" />;
